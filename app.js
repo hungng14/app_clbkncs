d47631909d21939abba3c73611c4479d0446272b
hungnv1 <hungnv1>
2019-07-13 14:08:20 +0700

    update icon

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const adminRouter = require('./routes/admin/admin');
 
 const app = express();
 
+require('./configs/database');
+
 
 // view engine setup
 app.engine('hbs', hbs({
@@ -46,7 +48,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(cors());
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'logo_club.ico')));
 app.use(logger('dev'));
 app.use(express.json());
 // app.use(express.urlencoded({
